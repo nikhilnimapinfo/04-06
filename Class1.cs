c1bdd096ecb7e361a164cc0041d7e00b113ff185
nikhilnimapinfo <166018854+nikhilnimapinfo@users.noreply.github.com>
2024-06-04 19:00:17 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewBagAndViewData.Models
+{
+    public class Class1
+    {
+        public int   Id { get; set; }
+        public string Name { get; set; }
+        public string Addr { get; set; }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewBagAndViewData.Models;
+
+namespace ViewBagAndViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Thane And Mumbai IS Near By";
+            ViewBag.MyList=new List<string>() { "Thane","Mumbai","Mulund","GhatKoper","Panvel","Badlapur"};
+
+
+            List<Class1> list = new List<Class1>() { 
+                new Class1()
+                {
+                    Id =10,
+                    Name="AA",
+                    Addr="Mumbai"
+                }
+
+                ,
+                new Class1()
+                {
+                    Id =11,
+                    Name="BB",
+                    Addr="Thane"
+                },
+                new Class1()
+                {
+                    Id =12,
+                    Name="CC",
+                    Addr="Mulund"
+                }
+
+            };
+            ViewBag.NewData=list;
+            return View();
+        }
+    }
+}
